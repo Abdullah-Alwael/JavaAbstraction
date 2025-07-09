5a700b5d3626e8c4b987f5a7def5e96b15394e2a
Abdullah-Alwael <abdullah.alwael@outlook.sa>
2025-07-09 14:28:28 +0300

    Q1 with movie examples

--- src/com/abstraction/Main.java
+++ src/com/abstraction/Main.java
@@ -2,6 +2,7 @@ package com.abstraction;
 
 public class Main {
     public static void main(String[] args) {
+        // all names are imaginary (no Ai this time)
         Book b1 = new Book();
         Book b2 = new Book("Mathematics: introduction", 130.57,"Dr. Maths");
         Book b3 = new Book("Algorithms: final proofs", 650.99, "Dr. Algo");
@@ -21,6 +22,38 @@ public class Main {
         System.out.println("The price of book 5 titled: "+b5.getName()+"is"+b5.getPrice());
         b5.setPrice(b5.getPrice()-b5.getDiscount());
         System.out.println("The price changed to "+b5.getPrice()+" after discount");
+
+        Movie m1 = new Movie();
+        Movie m2 = new Movie("The must watch", 55.3, "The best director");
+        Movie m3 = new Movie("Sudden Awakening", 99.9,"Mr. Nightmare");
+        Movie m4 = new Movie();
+        Movie m5 = new Movie("Full boost", 78.6, "Dr. turbo");
+
+        m1.setName("First walk: step one");
+        System.out.println("The name of the first movie was:"+m1.getName());
+
+        System.out.println("The price of the second movie titled: "+m2.getName()+" was: "+m2.getPrice());
+        System.out.println("The discount: "+m2.getDiscount());
+        m2.setPrice(m2.getPrice()-m2.getDiscount());
+        System.out.println("The second movie has lower price for a limited time only!");
+        System.out.println("Rent now for a price of just: "+m2.getPrice());
+
+        System.out.println("The director of movie 3 titled: "+m3.getName()+" is: "+m3.getDirector());
+        m3.setDirector("Mr. The next budget");
+        System.out.println("The studio decided to change the director to: "+m3.getDirector()+" for financial reasons");
+
+        System.out.println("We also offer other movies: ");
+
+        m4.setName("The Other Movie");
+        m4.setDirector("Mr. That director once was");
+        m4.setPrice(97.2);
+
+        System.out.println("Movie 4 title: "+m4.getName()+" price: "+ m4.getPrice()+" Director: "+m4.getDirector());
+        System.out.println("Movie 4 has a discount of: "+m4.getDiscount());
+
+        System.out.println("Movie 5 title: "+m5.getName()+" price: "+ m5.getPrice()+" Director: "+m5.getDirector());
+        System.out.println("Movie 5 has a discount of: "+m5.getDiscount());
+
 
     }
 }
